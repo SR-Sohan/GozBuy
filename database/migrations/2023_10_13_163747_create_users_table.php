8f36388578dfd650fa447d02a242a7e6319706b5
SR-Sohan <sr.sohanur22@gmail.com>
2023-10-13 22:55:24 +0600

    migration done

--- database/migrations/2023_10_13_163747_create_users_table.php
+++ database/migrations/2023_10_13_163747_create_users_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string("name");
+            $table->string("email")->unique();
+            $table->string("password",200);
+            $table->enum("role",["admin","user"]);
+            $table->string("otp")->default(0);
             $table->timestamps();
         });
     }
